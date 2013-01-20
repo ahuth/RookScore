535b1ea53680c092d7d27402f3672fe0ddeb1bb9
Andrew Huth <andrewhuth2@live.com>
2013-01-19 19:57:18 -0500

    Keep score label within rect bounds

--- RookScore/RSProgressCircle.m
+++ RookScore/RSProgressCircle.m
@@ -11,7 +11,7 @@
 @interface RSProgressCircle ()
 
 - (CGFloat)degreesToRadians:(NSInteger)degrees;
-- (void)drawCenteredText:(NSString*)text atPoint:(CGPoint)point font:(UIFont*)textFont;
+- (void)drawCenteredText:(NSString*)text atPoint:(CGPoint)point withFont:(UIFont*)textFont inRect:(CGRect)aRect;
 - (CGPoint)getPointFromCenter:(CGPoint)center atDegrees:(NSInteger)degrees fromRadius:(CGFloat)radius;
 
 @end
@@ -39,13 +39,13 @@ const NSInteger CIRCLE_STROKE_WIDTH = 20;
     
     NSInteger direction;
     CGContextRef context = UIGraphicsGetCurrentContext();
+    NSString *scoreString = [NSString stringWithFormat:@"%d", _score];
+    NSString *teamString = [NSString stringWithFormat:@"Team %d", _team];
     
     // Circle dimensions
     CGRect allRect = self.bounds;
     CGPoint center = CGPointMake(allRect.size.width / 2, allRect.size.height / 2);
     CGFloat radius = (allRect.size.width - CIRCLE_STROKE_WIDTH) / 2;
-    
-    [self drawCenteredText:[NSString stringWithFormat:@"Team %d", _team] atPoint:center font:[UIFont boldSystemFontOfSize:20.0]];
     
     // The start and end angle to draw between.  Notice that we start at -90
     // degrees, which is straight up.
@@ -66,22 +66,65 @@ const NSInteger CIRCLE_STROKE_WIDTH = 20;
     CGContextSetLineWidth(context, CIRCLE_STROKE_WIDTH);
     CGContextAddArc(context, center.x, center.y, radius, [self degreesToRadians:startAngle], [self degreesToRadians:endAngle], direction);
     CGContextStrokePath(context);
-
-    // Draw the text.
-    NSString *scoreString = [NSString stringWithFormat:@"%d", _score];
-    //[self drawCenteredText:scoreString atPoint:center font:[UIFont boldSystemFontOfSize:16.0]];
-    CGPoint textPoint = [self getPointFromCenter:center atDegrees:endAngle fromRadius:radius];
-    [self drawCenteredText:scoreString atPoint:textPoint font:[UIFont boldSystemFontOfSize:16.0]];
+    
+    // Draw text to identify the team and the score.  Where we draw these depends
+    // on if the game is starting, in progress, or over.
+    if (_progress == 0) {
+        
+        // Game is just starting, so only draw the team label.
+        [self drawCenteredText:teamString
+                       atPoint:center
+                      withFont:[UIFont boldSystemFontOfSize:20.0]
+                        inRect:allRect];
+        
+    } else if (_progress < 1) {
+        
+        // Offset the text we'll draw by 10 degrees from the edge of our partial
+        // circle, so that we're not drawing text over the circle.
+        CGPoint textPoint = [self getPointFromCenter:center
+                                           atDegrees:(endAngle + 10)
+                                          fromRadius:radius];
+        
+        [self drawCenteredText:teamString
+                       atPoint:center
+                      withFont:[UIFont boldSystemFontOfSize:20.0]
+                        inRect:allRect];
+        
+        [self drawCenteredText:scoreString
+                       atPoint:textPoint
+                      withFont:[UIFont boldSystemFontOfSize:20.0]
+                        inRect:allRect];
+        
+    } else {
+        
+        // Game is over, so just draw the score.
+        [self drawCenteredText:scoreString
+                       atPoint:center
+                      withFont:[UIFont boldSystemFontOfSize:20.0]
+                        inRect:allRect];
+    }
 }
 
-- (void)drawCenteredText:(NSString*)text atPoint:(CGPoint)point font:(UIFont*)textFont {
+- (void)drawCenteredText:(NSString*)text atPoint:(CGPoint)point withFont:(UIFont*)textFont inRect:(CGRect)aRect {
     
-    // Find the width of our text.
     CGSize textSize = [text sizeWithFont:textFont];
     
-    // Adjust where we draw the text, based on its width, so that it appears
-    // centered on the point.
+    // Set where we draw the text so that it's centered on the point parameter.
     CGPoint textPoint = CGPointMake(point.x - textSize.width / 2, point.y - textSize.height / 2);
+    
+    // Adjust the point, if necessary, to be within x-axis of aRect.
+    if (textPoint.x < 0) {
+        textPoint.x = 0;
+    } else if (textPoint.x + textSize.width > aRect.size.width) {
+        textPoint.x = aRect.size.width - textSize.width;
+    }
+
+    // Adjust the point, if necessary, to be within y-axis of aRect.
+    if (textPoint.y < 0) {
+        textPoint.y = 0;
+    } else if (textPoint.y + textSize.height > aRect.size.height) {
+        textPoint.y = aRect.size.height - textSize.height;
+    }
     
     // Now draw it.
     [text drawAtPoint:textPoint withFont:textFont];
@@ -98,6 +141,8 @@ const NSInteger CIRCLE_STROKE_WIDTH = 20;
     
     // Get the coordinates of a point on a circle.
     
+    degrees = (_score < 0) ? -degrees : degrees;
+
     CGFloat x = center.x + radius * cos([self degreesToRadians:degrees]);
     CGFloat y = center.y + radius * sin([self degreesToRadians:degrees]);
     
